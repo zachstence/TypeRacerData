1015c89e9879a2f65d9cb266385d1e7896fc1500
Zach <zachstence@gmail.com>
2021-06-30 22:02:16 -0500

    Configure CORS Anywhere to use localhost when running in dev

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import replace from '@rollup/plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -75,7 +76,11 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser(),
+
+		replace({
+			'process.env.ENVIRONMENT': JSON.stringify( 'production' )
+		})
 	],
 	watch: {
 		clearScreen: false
--- src/TypeRacer.ts
+++ src/TypeRacer.ts
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 export class TypeRacer {
 
-    private static CORS_ANYWHERE = "https://zach08-cors-anywhere.herokuapp.com/";
+    private static CORS_ANYWHERE = process.env.ENVIRONMENT === "dev"
+        ? "https://zach08-cors-anywhere.herokuapp.com/"
+        : "http://localhost:8080/";
+
 
     static async getRaces(playerId: string, universe: string, numRaces: number) {
         return await axios.get(
