3843175c16888f1f266e23ea58093223ea28289c
Zach <zachstence@gmail.com>
2021-07-02 13:00:04 -0500

    Fix backwards config for dev/prod cors anywhere service

--- src/TypeRacer.ts
+++ src/TypeRacer.ts
@@ -3,14 +3,11 @@ import axios from 'axios';
 export class TypeRacer {
 
     private static CORS_ANYWHERE = process.env.ENVIRONMENT === "dev"
-        ? "https://zach08-cors-anywhere.herokuapp.com/"
-        : "http://localhost:8080/";
+        ? "http://localhost:8080/"
+        : "https://zach08-cors-anywhere.herokuapp.com/";
 
 
     static async getRaces(playerId: string, universe: string, numRaces: number) {
-        console.log(`ENVIRONMENT: ${process.env.ENVIRONMENT}`);
-        console.log(`Using CORS Anywhere URL: ${this.CORS_ANYWHERE}`);
-        
         return await axios.get(
             `${TypeRacer.CORS_ANYWHERE}https://data.typeracer.com/games?playerId=tr:${playerId}&universe=${universe ? universe : "play"}&n=${numRaces ? numRaces : 10}`,
         );
