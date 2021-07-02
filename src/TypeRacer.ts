f973d55b983639569b7b47d97843038bd8593097
Zach <zachstence@gmail.com>
2021-07-02 12:58:17 -0500

    Dummy changes to make gh-pages redeploy

--- src/TypeRacer.ts
+++ src/TypeRacer.ts
@@ -8,6 +8,9 @@ export class TypeRacer {
 
 
     static async getRaces(playerId: string, universe: string, numRaces: number) {
+        console.log(`ENVIRONMENT: ${process.env.ENVIRONMENT}`);
+        console.log(`Using CORS Anywhere URL: ${this.CORS_ANYWHERE}`);
+        
         return await axios.get(
             `${TypeRacer.CORS_ANYWHERE}https://data.typeracer.com/games?playerId=tr:${playerId}&universe=${universe ? universe : "play"}&n=${numRaces ? numRaces : 10}`,
         );
